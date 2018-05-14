9b8ecae54af0b0c5a38e080ec8468fd3735abb67
guskoro <34765935+guskoro@users.noreply.github.com>
2018-05-14 23:44:49 +0700

    update form

---
+++ src/servlet/admin/size_produk/AddSizeProdukServlet.java
@@ -0,0 +1,10 @@
+@javax.servlet.annotation.WebServlet(name = "AddSizeProdukServlet")
+public class AddSizeProdukServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+
+    }
+}
